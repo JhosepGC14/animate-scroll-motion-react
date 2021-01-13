1bd4d2f4a47a16066cdccecf3116bac95bfcaf26
jhosep.guadalupe <jhosep.guadalupe@panbox.pe>
2021-01-13 18:04:17 -0500

    animation scroll with frame-motion

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { motion } from "framer-motion";
+
+function App() {
+  return (
+    <div className="container">
+      <article className="info">
+        <header className="header">
+          <span className="subtitle">React Animation</span>
+          <h1 className="title">Reveal on Scroll-up</h1>
+        </header>
+        <section className="content">
+          <p>
+            Using Framer Motion, the actions bar at the bottom of the screen
+            only fades in when scrolled up and fades out whenever we scroll
+            down.
+          </p>
+        </section>
+      </article>
+      <Actions />
+    </div>
+  );
+}
+
+function Actions() {
+  const [lastYPos, setLastYPos] = React.useState(500);
+  const [shouldShowActions, setShouldShowActions] = React.useState(false);
+
+  React.useEffect(() => {
+    function handleScroll() {
+      const yPos = window.scrollY;
+      console.log("scroll : ", yPos);
+      const isScrollingUp = yPos > lastYPos;
+      console.log("lastYPos : ", lastYPos);
+      console.log("isScrollingUp : ", isScrollingUp);
+
+      setShouldShowActions(isScrollingUp);
+
+    }
+
+    window.addEventListener("scroll", handleScroll, false);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll, false);
+    };
+  }, [lastYPos]);
+
+  return (
+    <motion.div
+      className="actions"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: shouldShowActions ? 1 : 0 }}
+      transition={{ opacity: { duration: 2.0 } }}
+    >
+      <button>
+        <span className="fas fa-share-square" />
+      </button>
+      <button>
+        <span className="fas fa-user" />
+      </button>
+      <button>
+        <span className="fas fa-times" />
+      </button>
+    </motion.div>
+  );
+}
+
+export default App;
